2731298042f6a7a835af4c4cb2840a3b1f58667f
AdriF31 <adrifatwa1@gmail.com>
2020-06-22 19:24:35 +0700

    add page routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:zakats/pages/home.dart';
+import 'package:zakats/pages/muzaqi.dart';
 import 'package:zakats/pages/splashscreen.dart';
+import 'package:zakats/pages/zakat_fitrah.dart';
+import 'package:zakats/pages/zakat_maal.dart';
+import 'package:zakats/pages/zakatpedia.dart';
 
 
 void main() {
@@ -16,6 +20,11 @@ class MyApp extends StatelessWidget {
         home: SplashScreenPage(),
         routes: <String, WidgetBuilder>{
           "/home" : (BuildContext context) => HomePage(),
+          "/zakatpedia" : (BuildContext context) => Zakatpedia(),
+          "/zakatfitrah" : (BuildContext context) => ZakatFitrah(),
+          "/zakatmaal" : (BuildContext context) => ZakatMaal(),
+          "/muzaqi" : (BuildContext context) => Muzaqi(),
+
         },
         theme: ThemeData(
           primaryColor: Color(0xFF56A74D),
